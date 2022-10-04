733fc5abc8bd9cbd1572bd5021d256bf6e53b8b0
DoEpgobXD <112198898+DoEpgobXD@users.noreply.github.com>
2022-10-04 13:22:42 -0500

    Test work

---
+++ Test/Largest-Number-In-erray/Index.js
@@ -0,0 +1,8 @@
+function findLargestNumber(mainArray) {
+  return mainArray.map(function (subArray){
+    return subArray.reduce(function (previousLargestNumber, currentLargestNumber) {
+      return (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
+    }, 0);
+  });
+}
+console.log = findLargestNumber ([5,2,8,3,12,0,4])
---
+++ Test/data-types/Index.js
@@ -0,0 +1,6 @@
+console.log=undefined
+console.log = Array
+console.log={}
+console.log=true,false
+console.log=5
+console.log='Hello'
