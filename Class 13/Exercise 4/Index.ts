ea705067f3235c0e485c4e82046364177c3e8576
DoEpgobXD <112198898+DoEpgobXD@users.noreply.github.com>
2022-10-11 14:24:05 -0500

    Class 13 finish

---
+++ Class 13/Exercise 4/Index.ts
@@ -0,0 +1,13 @@
+class human {
+    birthYear: Number = 0
+    static birhtYear: number
+    ageDifference(){
+        console.log("The age difference between my animal and I is 15 years")
+    }
+}
+class animal {
+    birthYear: Number = 0
+    static birthYear: number
+}
+animal.birthYear= 2005
+human.birhtYear= 2000
